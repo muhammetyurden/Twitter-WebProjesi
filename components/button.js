9c3c1cc0ec53dbdbabd682f0c328e7394c02f382
muhammetyurden <56937631+muhammetyurden@users.noreply.github.com>
2021-01-12 20:26:28 +0300

    button için seçenek eklendi

--- components/button.js
+++ components/button.js
@@ -2,18 +2,35 @@ import React from 'react'
 
 //butonların üzerinde iken pointer olma
 import cn from 'classnames'
+import Link from 'next/link'
 
 import styles from './button.module.css'
 
-function Button({ full = false, children, className, ...props }) {
+function LinkButton({ href, children, ...props }) {
   return (
-    <button
-      type="button"
+    <Link href={href}>
+      <a {...props}>{children}</a>
+    </Link>
+  )
+}
+
+function BaseButton({ children, ...props }) {
+  return (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  )
+}
+
+function Button({ full = false, children, className, ...props }) {
+  const Comp = props.href ? LinkButton : BaseButton
+  return (
+    <Comp
       className={cn(styles.button, full && styles.fullWidht, className)}
       {...props}
     >
       {children}
-    </button>
+    </Comp>
   )
 }
 
