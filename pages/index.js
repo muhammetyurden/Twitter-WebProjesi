d2d52ad35e3dadb990345adebe687deefc474bd8
muhammetyurden <56937631+muhammetyurden@users.noreply.github.com>
2021-01-13 12:19:14 +0300

    örnek tweet olusturuldu.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Tweet from '../components/tweet'
 
 function Homepage() {
-  return <Layout>Anasayfa</Layout>
+  return (
+    <Layout>
+      <Tweet
+        name="Muhammet Yürden"
+        slug="muhammet.rdd"
+        datetime={new Date('2021-01-13')}
+        text={`“İnce düşünen insanların sık yaptığı hata, kişiliği küçük insanlara büyük anlamlar yüklemektir.”
+
+|  Aldous Huxley`}
+      />
+
+      <Tweet
+        name="Muhammet Yürden"
+        slug="muhammet.rdd"
+        datetime={new Date('2021-01-13')}
+        text={`Peru'da bir mahkeme, Coronavirüs’ü Bill Gates ve Rockefeller ailesininin ürettiğini iddia edip, davaların gecikmesi nedeniyle soruşturma açtı. 
+
+— 20Minitos`}
+      />
+    </Layout>
+  )
 }
 
 export default Homepage
